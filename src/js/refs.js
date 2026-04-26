d33341a2d81a6b3402842857c241036e7dbaf3ca
Volodymyr Andrusenko <vovchikwoolf007@gmail.com>
2026-04-26 15:32:27 +0200

    delet-task

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,6 @@
 export default {
   formEl: document.querySelector('.header-form'),
   listEl: document.querySelector('.tasks-list'),
+  changeThemBtn: document.querySelector('.theme-toggle-button'),
+  body: document.querySelector('body')
 };
